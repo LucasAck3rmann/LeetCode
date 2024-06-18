1f3cb73b01b37bce0a7a088e57853e6e0da7de48
Lucas Ackermann <ackermannlucas25@gmail.com>
2024-06-17 23:08:18 -0300

    LeetCode

---
+++ exercicio_15.js
@@ -0,0 +1,12 @@
+//6) Dado uma lista com as notas tiradas pelos alunos. imprima a maior nota.Ex de lista de notas = [2, 7, 3, 8, 10, 4]
+
+let notas = [2, 7, 3, 8, 10, 4];
+let maiorNota = notas[0];
+
+for (let i = 0; i < notas.length; i++) {
+  if (notas[i] > maiorNota) {
+    maiorNota = notas[i];
+  }
+}
+
+console.log(maiorNota);
